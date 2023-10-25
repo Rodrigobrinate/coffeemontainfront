b8d56d165d4e69cc7739c7a72469bd057ae2747a
rodrigo <rbp20199@gmail.com>
2023-10-25 09:57:07 -0300

    images added

---
+++ src/pages/privacity/index.tsx
@@ -0,0 +1,178 @@
+import Header from '@/components/header';
+import React from 'react';
+
+// import { Container } from './styles';
+
+const privacity: React.FC = () => {
+  return (
+  
+  <div className='w-4/5 mx-auto'>
+    <Header />
+    <h1 className='text-center my-4 text-2xl font-bold'>Política de privcidade</h1>
+
+Política de Privacidade - Plano de Assinatura de Café Especial
+Este site é mantido e operado por COFFEE MOUNTAIN CAPARAO, CNPJ de nº 50.155.204/0001-70, nós coletamos e utilizamos alguns dados pessoais que pertencem àqueles que utilizam nosso site. Ao fazê-lo, agimos na qualidade de controlador desses dados e estamos sujeitos às disposições da Lei Federal nº 13.709/2018 (Lei Geral de Proteção de Dados Pessoais – LGPD). 
+Esta Política de Privacidade descreve como a COFFEE MOUNTAIN CAPARAO coleta, utiliza e protege as informações dos clientes que utilizam nosso serviço de plano de assinatura de café especial.
+1. Quem deve utilizar o nosso site
+Nosso site deve ser utilizado por pessoas com mais de dezoito anos de idade. Sendo assim, crianças e adolescentes devem utilizá-lo apenas com a supervisão de adultos.
+<br /><br />
+2. Informações Coletadas
+Nosso site coleta e utiliza alguns dados pessoais de nossos usuários, de acordo com o disposto nesta seção.
+<br /><br />
+2.1. Informações Pessoais:
+Coletamos informações pessoais, como seu nome, endereço, endereço de e-mail e informações de pagamento, para processar suas assinaturas e fornecer o serviço.
+<br /><br />
+2.2. Informações de Uso:
+Coletamos informações sobre a forma como você interage com nosso site e serviço, incluindo os produtos que você visualiza, compra e avalia.
+<br /><br />
+3. Uso das Informações
+<br /><br />
+3.1. Processamento de Assinaturas:
+Utilizamos suas informações pessoais para processar suas assinaturas, gerenciar sua conta e fornecer o serviço de café especial.
+<br /><br />
+3.2. Melhoria do Serviço:
+Analisamos as informações de uso para melhorar nosso site, personalizar sua experiência e otimizar nosso catálogo de produtos.
+<br /><br />
+3.3. Comunicações e Atendimento ao Cliente:
+Utilizamos seu endereço de e-mail para enviar informações relacionadas ao serviço, como confirmações de pedidos, atualizações e informações sobre promoções. Também usamos suas informações para responder a suas dúvidas e fornecer suporte ao cliente.
+<br /><br />
+4. Compartilhamento de Informações
+4.1. Terceiros:
+Não compartilhamos suas informações pessoais com terceiros sem seu consentimento, exceto quando necessário para fornecer o serviço (por exemplo, processadores de pagamento).
+<br /><br />
+5. Segurança das Informações
+5.1. Proteção de Dados:
+Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado e uso indevido.
+<br /><br />
+6. Seus Direitos e Controle
+6.1. Acesso e Atualização:
+Você pode acessar e atualizar suas informações pessoais através da sua conta no site.
+<br /><br />
+6.2. Cancelamento:
+Você pode cancelar sua assinatura a qualquer momento através da sua conta ou entrando em contato conosco.
+<br /><br />
+7. Menores de Idade
+7.1 Restrição de Idade:
+O serviço não é destinado a menores de 18 anos. Não coletamos intencionalmente informações de menores de idade.
+<br /><br />
+8. Alterações na Política de Privacidade
+8.1. Atualizações:
+Podemos atualizar esta Política de Privacidade ocasionalmente. Recomendamos que você revise esta política periodicamente para estar ciente de quaisquer alterações.
+<br /><br />
+9. Contato
+9.1. Dúvidas:
+Se você tiver dúvidas ou preocupações sobre esta Política de Privacidade, entre em contato conosco pelo e-mail coffeemountaincaparao@coffeemountaincaparao.com.br.
+10. Coleta de dados não previstos expressamente
+Eventualmente, outros tipos de dados não previstos expressamente nesta Política de Privacidade poderão ser coletados, desde que sejam fornecidos com o consentimento do usuário, ou, ainda, que a coleta seja permitida com fundamento em outra base legal prevista em lei. Em qualquer caso, a coleta de dados e as atividades de tratamento dela decorrentes serão informadas aos usuários do site.
+
+
+<br /><br /><br /><br />
+Política de Uso de Cookies
+<br /><br />
+Esta Política de Uso de Cookies descreve como utilizamos cookies e tecnologias semelhantes em nosso site para melhorar sua experiência de navegação. Ao acessar nosso site, você concorda com o uso de cookies de acordo com esta política.
+<br /><br />
+O que são Cookies?
+<br /><br />
+Cookies são pequenos arquivos de texto que são armazenados em seu dispositivo (computador, tablet, smartphone) quando você visita um site. Eles são amplamente utilizados para tornar os sites funcionais ou mais eficientes, além de fornecer informações aos proprietários do site.
+<br /><br />
+Como Utilizamos os Cookies
+<br /><br />
+Utilizamos cookies para os seguintes propósitos:
+<br /><br />
+Melhoria da Experiência do Usuário: Utilizamos cookies para entender como os visitantes interagem com nosso site, permitindo-nos aprimorar a navegação, a interface e o conteúdo, proporcionando uma experiência mais personalizada.
+<br /><br />
+Análise e Estatísticas: Utilizamos cookies para coletar dados estatísticos e analíticos sobre o uso do nosso site, ajudando-nos a entender como os usuários interagem com o conteúdo, quais páginas são mais visitadas e como podemos melhorar nosso site.
+<br /><br />
+Personalização de Conteúdo: Os cookies nos permitem adaptar o conteúdo exibido a você com base em suas preferências e comportamentos de navegação anteriores.
+<br /><br />
+Publicidade Direcionada: Podemos utilizar cookies para exibir anúncios relevantes com base em seus interesses, melhorando a eficiência e eficácia de nossas campanhas publicitárias.
+<br /><br />
+Tipos de Cookies Utilizados
+<br /><br />
+Cookies Necessários: Essenciais para o funcionamento do site e para fornecer os serviços que você solicitou. Sem esses cookies, o site não pode ser fornecido.
+<br /><br />
+Cookies de Desempenho: Coletam informações sobre como os visitantes usam o site, por exemplo, quais páginas são mais visitadas. Esses cookies não coletam informações que identificam um visitante.
+<br /><br />
+Cookies de Funcionalidade: Permitem que o site se lembre das escolhas que você faz (como seu nome de usuário, idioma ou região) e oferecem funcionalidades aprimoradas e mais personalizadas.
+<br /><br />
+Cookies de Publicidade: São usados para entregar anúncios mais relevantes para você e seus interesses. Eles também são usados para limitar o número de vezes que você vê um anúncio e ajudam a medir a eficácia das campanhas publicitárias.
+<br /><br />
+Como Gerenciar os Cookies
+<br /><br />
+Você pode gerenciar suas preferências de cookies através das configurações do seu navegador. A maioria dos navegadores permite que você aceite, recuse ou exclua cookies. No entanto, ao desativar os cookies, algumas funcionalidades do site podem não funcionar corretamente.
+<br /><br />
+Ao continuar a usar nosso site sem alterar suas configurações de cookies, você concorda com nosso uso de cookies conforme descrito nesta Política de Uso de Cookies.
+<br /><br />
+Esta política pode ser atualizada periodicamente para refletir as mudanças em nossas práticas de cookies. Recomendamos que você reveja esta política regularmente para estar atualizado sobre qualquer alteração.
+<br /><br />
+Última atualização: 25/10/2023
+<br /><br />
+Se tiver alguma dúvida ou preocupação sobre nossa política de cookies, entre em contato conosco através dos dados fornecidos em nossa página de contato.
+
+
+
+
+
+
+
+
+
+
+
+
+
+<br /><br /><br /><br />
+Termos de Uso e Contrato de Prestação de Serviço para Assinatura de Café Especial
+<br /><br />
+1. Introdução
+Bem-vindo(a) ao nosso site de assinatura de café especial. Antes de utilizar nossos serviços, é importante que você leia e compreenda os seguintes Termos de Uso e Contrato de Prestação de Serviço. Ao utilizar nosso site e realizar a assinatura, você concorda com todos os termos aqui estabelecidos.
+<br /><br />
+2. Definições
+<br /><br />
+2.1. Assinante: Refere-se ao cliente que adquire a assinatura mensal ou anual do café especial oferecido em nosso site.
+2.2. Site: Refere-se ao nosso website, por meio do qual os serviços de assinatura de café especial são disponibilizados.
+<br /><br />
+3. Assinatura de Café Especial
+3.1. Condições de Assinatura:
+A assinatura é mensal ou anual, de acordo com a escolha do Assinante.
+O pagamento será realizado de forma recorrente no valor estipulado no ato da assinatura.
+O Assinante concorda em fornecer informações precisas e atualizadas para processamento do pagamento.
+<br /><br />
+3.2. Renovação e Cancelamento:
+As assinaturas serão renovadas automaticamente no final do período contratado, a menos que o Assinante opte pelo cancelamento.
+O Assinante pode cancelar sua assinatura a qualquer momento antes do próximo ciclo de faturamento.
+<br /><br />
+4. Responsabilidades do Assinante
+4.1. Informações Pessoais:
+<br /><br />
+O Assinante deve fornecer informações precisas e atualizadas, incluindo dados de pagamento e endereço de entrega.
+<br /><br />
+4.2. Uso Adequado:
+O Assinante concorda em utilizar o serviço de assinatura de café especial de acordo com as orientações fornecidas no site, respeitando as leis e regulamentações aplicáveis.
+5. Responsabilidades do Site
+5.1. Fornecimento do Café Especial:
+O Site se compromete a fornecer o café especial conforme especificado na assinatura do Assinante.
+<br /><br />
+5.2. Atendimento ao Cliente:
+O Site disponibiliza um canal de atendimento ao cliente para sanar dúvidas e resolver questões relacionadas à assinatura.
+<br /><br />
+6. Limitação de Responsabilidade
+O Site não será responsável por quaisquer danos diretos, indiretos, incidentais, consequenciais ou punitivos decorrentes do uso ou impossibilidade de uso dos serviços de assinatura de café especial.
+<br /><br />
+7. Disposições Gerais
+<br /><br />
+7.1. Alterações nos Termos de Uso:
+O Site reserva o direito de modificar estes Termos de Uso a qualquer momento, sendo recomendável que o Assinante os revise periodicamente.
+<br /><br />
+7.2. Lei Aplicável:
+Estes Termos de Uso serão regidos pelas leis do Brasil.
+Ao aceitar estes Termos de Uso e Contrato de Prestação de Serviço, o Assinante está formalizando um contrato legal e concorda em cumprir as disposições aqui estabelecidas.
+<br /><br />
+Data de entrada em vigor: 25/10/2023
+<br /><br />
+Se você tiver alguma dúvida ou preocupação, entre em contato conosco através dos meios de comunicação disponíveis em nosso site.
+
+  </div>);
+}
+
+export default privacity;
